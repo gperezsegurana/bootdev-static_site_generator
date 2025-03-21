7d948d44a322a1be0f650f6ed2b7ee87a0cc863f
gperezse <gustavo.perez.segurana@cern.ch>
2025-03-21 19:43:20 +0100

    Add unit tests for TextNode class

---
+++ src/test_textnode.py
@@ -0,0 +1,39 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_neq_different_content(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is another text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_neq_different_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_empty_content(self):
+        node = TextNode("", TextType.BOLD)
+        node2 = TextNode("", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_empty_content_different_type(self):
+        node = TextNode("", TextType.BOLD)
+        node2 = TextNode("", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_link_null_url(self):
+        node = TextNode("This is a link", TextType.LINK, url=None)
+        node2 = TextNode("This is a link", TextType.LINK)
+        self.assertEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
